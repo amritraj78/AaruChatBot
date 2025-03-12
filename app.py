d6f8f3a9b3b685e35a70e57a856d85535c2e2513
𝐀ᴍʀɪᴛ 𝐑ᴀᴊ <155341830+amritraj78@users.noreply.github.com>
2025-03-12 06:02:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
+# Get the port from the environment variable or default to 8000
 port = int(os.environ.get('PORT', 8000))
 
 @app.route('/')
@@ -10,4 +11,5 @@ def index():
     return 'Hello user.'
 
 if __name__ == '__main__':
+    # Run the app on all available IPs (0.0.0.0) and the specified port
     app.run(host='0.0.0.0', port=port)
